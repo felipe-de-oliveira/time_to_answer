e435ec556082534fb56274cbd56ab08c32cf0dd2
Felipe Oliveira <felipeoliveiraoliveira92@gmail.com>
2021-12-16 13:56:42 -0300

    adicionando outro tema para usuarios

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,8 +12,13 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 
-# app/asserts
-Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css )
+# app/assers
+Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css 
+                                                  users_backoffice.js users_backoffice.css)
 
-# lib/asserts 
-Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css )
+# lib/assers 
+Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css
+                                                  custom.js custom.css
+                                                  img.jpg )
+
+Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js )
