7ea9f24038d574888451051b75aab856ea04eee1
Felipe Oliveira <felipeoliveiraoliveira92@gmail.com>
2022-01-03 14:35:58 -0300

    finalizando o formulario de ediçao do admin

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   
   namespace :admins_backoffice do
     get 'welcome/index' # dashboard
-    resources :admins, only: [:index, :edit] # admins
+    resources :admins, only: [:index, :edit, :update] # admins
   end
 
   devise_for :users
